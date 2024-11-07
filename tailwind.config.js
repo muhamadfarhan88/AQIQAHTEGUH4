372e60d6caf74fecaad31663ee8e5ec1c03cf576
MuhamadFarhan4455 <muhamadfarhan4455@gmail.com>
2024-11-08 01:30:41 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+      "orange" : {
+        100: '#FFEDD5',
+        200: '#FEF3C7',
+        300: '#FDE68A',
+        400: '#FCD34D',
+        500: '#FBBF24',
+        600: '#F59E0B',
+        700: '#D97706',
+        800: '#B45309',
+        900: '#92400E',
+        950: '#7A2A0D',
+      }
+    },
+  },
+  plugins: [],
+}
+}
